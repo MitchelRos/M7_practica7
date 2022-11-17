3e297dfeb7cf5d7ba629ce3f1cad6a7361ebdb22
ksama22 <ksama22@jaumebalmes.net>
2022-11-17 20:14:17 +0100

    create xml file wit 5 element

--- main_xml.py
+++ main_xml.py
@@ -1,30 +1,48 @@
 import xml.etree.ElementTree as ET
+#Crea fitxer
+def writeFile():
+    # Crea arxiu
+    f = open("arxiu.xml", "w")
+    f.write("<students></students>")
+    f.close()
+
+def creaStudent(pare, nom, cognom, email, dni, isMatriculat):
+    # Crea el fill dins del pare
+    fill = ET.SubElement(pare, "student")
+    fill.set("matriculat", isMatriculat)
+    # Crea <name>Kevin</name>
+    createName = ET.SubElement(fill, "name")
+    createName.text = nom
+    # Crea <surname>Sama</surname>
+    createSurname = ET.SubElement(fill, "surname")
+    createSurname.text = cognom
+    # Crea <email>kev@mail.com</email>
+    createEmail = ET.SubElement(fill, "email")
+    createEmail.text = email
+    # Crea <dni>kev@mail.com</dni>
+    createDNI = ET.SubElement(fill, "dni")
+    createDNI.text = dni
+    return fill
 
 
+# Crea un fitxer
+writeFile()
+
+#Seleciona l'arxiu
 tree = ET.parse("arxiu.xml")
 root = tree.getroot()
 # Crea el pare
+root =ET.Element("students")
+#Seleciona el pare
 pare = ET.Element("students")
 
-# Crea el fill dins del pare
-fill = ET.SubElement(pare, "student")
-fill.set("matriculat", "true")
 
-#Crea <name>Kevin</name>
-createName = ET.SubElement(fill, "name")
-createName.text = "Kevin"
 
-#Crea <surname>Sama</surname>
-createSurname = ET.SubElement(fill, "surname")
-createSurname.text = "Sama"
-
-#Crea <email>kev@mail.com</email>
-createEmail = ET.SubElement(fill, "email")
-createEmail.text = "kev@mail.com"
-
-#Crea <dni>kev@mail.com</dni>
-createDNI = ET.SubElement(fill, "dni")
-createDNI.text = "12345678x"
+creaStudent(pare, nom="Kevin", cognom="Sama", email="kev@gmail.com", dni="1111x", isMatriculat="false")
+creaStudent(pare, nom="Mitchel", cognom="Sama", email="kev@gmail.com", dni="2222d", isMatriculat="false")
+creaStudent(pare, nom="Julio", cognom="Sama", email="kev@gmail.com", dni="333f", isMatriculat="false")
+creaStudent(pare, nom="Luis", cognom="Sama", email="kev@gmail.com", dni="444f", isMatriculat="false")
+creaStudent(pare, nom="Jorge", cognom="Sama", email="kev@gmail.com", dni="5555x", isMatriculat="false")
 
 ET.indent(pare)
 ET.dump(pare)
